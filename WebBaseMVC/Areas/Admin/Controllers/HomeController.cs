fe6168be5d220115edb404afc2b60cc3c47f82d2
Nickel <nickel264@gmail.com>
2017-04-15 00:25:33 +0700

    comit file

--- WebBaseMVC/Areas/Admin/Controllers/HomeController.cs
+++ WebBaseMVC/Areas/Admin/Controllers/HomeController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBaseMVC.Areas.Admin.Models.Bus;
 
 namespace WebBaseMVC.Areas.Admin.Controllers
 {
     public class HomeController : AuthorizeController
     {
+        
         //
         // GET: /Admin/Home/
         public ActionResult Index()
         {
-            return View(); 
+           
+            ViewBag.MainMenu = new BusMenu().GetMainMenu();
+            return View();
         }
+
+        [ChildActionOnly]
+        public PartialViewResult MainMenu()
+        {           
+            var res= new BusMenu().GetMainMenu();
+            return PartialView(res);            
+        }
+
+
+
+
 	}
 }
---
+++ WebBaseMVC/Areas/Admin/Models/Bus/BusMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebBaseMVC.Areas.Admin.Models.Framework;
+
+namespace WebBaseMVC.Areas.Admin.Models.Bus
+{    
+    public class BusMenu
+    {
+        WebBaseDbContext dbcontext = null;
+
+        public List<base_menu> GetMainMenu()
+        {
+            dbcontext = new WebBaseDbContext();
+            return dbcontext.base_menu.ToList();
+        }
+    }
+}
