56f06dfb4fb8729f3e131193a3951bfed7134b77
Nickel <nickel264@gmail.com>
2017-04-18 00:26:23 +0700

    Commit file NgGrid

---
+++ WebBaseMVC/Areas/Admin/Controllers/UserManageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBaseMVC.Areas.Admin.Controllers
+{
+    public class UserManageController : Controller
+    {
+        //
+        // GET: /Admin/UserManage/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
